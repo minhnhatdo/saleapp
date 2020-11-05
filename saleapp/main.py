49f9f2b3228d8d10f5de0e73f89c13e10a299656
minhnhat <1851050107nhat@ou.edu.vn>
2020-11-05 11:01:59 +0700

    BTH2

--- saleapp/main.py
+++ saleapp/main.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request
 from saleapp import app, utils
 
 
@@ -8,10 +8,17 @@ def index():
     return render_template('index.html', categories=categories)
 
 
-@app.route("/products")
+@app.route('/products')
 def product_list():
-    products = utils.ReadData('data/product.json')
+    cat_id = request.args.get('category_id')
+    products = utils.read_product(cat_id)
     return render_template('product_list.html', products=products)
+
+
+@app.route('/products/<int:product_id>')
+def product_detail(product_id):
+    product = utils.get_product_by_id(product_id)
+    return render_template('product_detail.html', product=product)
 
 
 if __name__ == "__main__":
--- saleapp/utils.py
+++ saleapp/utils.py
@@ -4,3 +4,18 @@ import json
 def ReadData(path = 'data/categories.json'):
     with open(path, encoding='utf-8') as f:
         return json.load(f)
+
+
+def get_product_by_id(product_id):
+    products = ReadData('data/product.json')
+    for p in products:
+        if p['id'] == product_id:
+            return p
+
+
+def read_product(cat_id=None):
+    products = ReadData('data/product.json')
+    if cat_id:
+        cat_id = int(cat_id)
+        products = [p for p in products if p['category_id'] == cat_id]
+    return products
